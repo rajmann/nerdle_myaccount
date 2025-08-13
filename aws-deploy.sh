8e60d3f7f31431ad4bb1a283a0024817e41bbd35
Marcus Tettmar <13750049-MarcusTettmar2@users.noreply.replit.com>
2025-08-13 14:46:15 +0000

    First commit

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,3 +1,3 @@
-aws s3 sync dist/public/ s3://nerdle-serverless-app/connect --acl public-read
+aws s3 sync build/ s3://nerdle-serverless-app/myaccount --acl public-read
 
-aws cloudfront create-invalidation --distribution-id E2GV7CU2G3KWL8 --paths "/connect/*" "/connect" "/connect*"
+aws cloudfront create-invalidation --distribution-id E2GV7CU2G3KWL8 --paths "/myaccount/*" "/myaccount" "/myaccount*"
